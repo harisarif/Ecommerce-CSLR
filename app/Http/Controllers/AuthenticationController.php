fe2a56824a1e3460f820820b40752459dea13d9b
Faraz <farazmehmood2563@gmail.com>
2026-03-12 14:19:30 +0500

    fix view file return issue for admin dashboard

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -9,17 +9,17 @@ class AuthenticationController extends Controller
 {
     public function forgotPassword()
     {
-        return view('authentication/forgotPassword');
+        return view('authentication.forgotPassword');
     }
 
     public function signIn()
     {
-        return view('authentication/signIn');
+        return view('authentication.signIn');
     }
 
     public function signUp()
     {
-        return view('authentication/signUp');
+        return view('authentication.signUp');
     }
 
 
